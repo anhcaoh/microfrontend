65bb70ef338ee6cd94d8c741893cd636b245f045
Anh Cao <anhcaoh@gmail.com>
2022-01-22 23:49:18 -0500

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+  return (
+    <>
+      <MarketingApp />
+      <h1>Hi there from App.js</h1>
+    </>
+  );
+};
